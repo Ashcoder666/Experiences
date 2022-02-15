b7e1ba60c86cad9243094c23ecaf938543a34994
Ashir N Basheer <ashirnbasheer@gmail.com>
2022-02-15 21:21:17 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from 'react'
+import Axios from "axios"
+import './app.css'
 
-function App() {
+const App = () => {
+    const [review,setReview] = useState([])
+    const [loading,setLoading] = useState(true);
+    const [val,setval] = useState(0)
+    const fetchReview = async() =>{
+        await Axios.get('http://localhost:3001/tour-api').then(response=>{
+         setReview(response.data)
+        })
+        setLoading(false)
+    }
+    useEffect(()=>{
+       
+        fetchReview();
+       
+    },[])
+
+
+
+    if(loading){
+        return(
+            <div>
+
+            <h1>Loading....</h1>
+            </div>
+        )
+    }
+    const {name,id,description,travelExpense,image} = review[val]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-body'>
+     <div className='button' >
+        {review.map((item,index)=>{
+            return <div> <button onClick={()=>{setval(index)}} >{item.name}</button></div>
+            
+        })}
+     </div>
+     <div className='details' >
+         {name}
+         {description}
+         {travelExpense}
+         <img src={image} alt={name} />
+
+     </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
